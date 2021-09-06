e1d942a88a7f6847ddb7d0e738949d2a3a0b6017
Naman Oli <53824950+NamanOli@users.noreply.github.com>
2021-09-06 14:01:54 +0530

    4. Longest Common Substring.cpp

--- DP/2. LCS/4. Longest Common Substring.cpp
+++ DP/2. LCS/4. Longest Common Substring.cpp
@@ -1,1 +1,58 @@
+// https://practice.geeksforgeeks.org/problems/longest-common-substring1452/1#
 
+#include<bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    
+    int longestCommonSubstr (string s1, string s2, int x, int y)
+    {
+        // your code here
+        int ans=0;
+        int dp[x+1][y+1];
+        for(int i=0;i<x+1;i++)
+        {
+            for(int j=0;j<y+1;j++)
+            {
+                if(i==0||j==0)
+                {
+                    dp[i][j]=0;
+                }
+            }
+        }
+        for(int i=1;i<x+1;i++)
+        {
+            for(int j=1;j<y+1;j++)
+            {
+                if(s1[i-1]==s2[j-1])
+                {
+                    dp[i][j]=1+dp[i-1][j-1];
+                    ans=max(ans,dp[i][j]);
+                }
+                else
+                {
+                    dp[i][j]=0;
+                }
+            }
+        }
+        return ans;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t; cin >> t;
+    while (t--)
+    {
+        int n, m; cin >> n >> m;
+        string s1, s2;
+        cin >> s1 >> s2;
+        Solution ob;
+
+        cout << ob.longestCommonSubstr (s1, s2, n, m) << endl;
+    }
+}
