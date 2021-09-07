bd19a48e5cbcc2d09ee4e563486f4faf3a1bffe5
Naman Oli <53824950+NamanOli@users.noreply.github.com>
2021-09-07 09:12:17 +0530

    9. Minimum Deletions.cpp

---
+++ DP/2. LCS/9. Minimum Deletions.cpp
@@ -0,0 +1,63 @@
+// https://practice.geeksforgeeks.org/problems/minimum-deletitions1648/1#
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+//User function template for C++
+
+class Solution{
+  public:
+  int lcs(int x, int y, string s1, string s2)
+    {
+        int dp[x+1][y+1];
+        for(int i=0;i<x+1;i++)
+        {
+            for(int j=0;j<y+1;j++)
+            {
+                if(i==0||j==0)
+                {
+                    dp[i][j]=0;
+                }
+            }
+        }
+        for(int i=1;i<x+1;i++)
+        {
+            for(int j=1;j<y+1;j++)
+            {
+                if(s1[i-1]==s2[j-1])
+                {
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else
+                {
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        return dp[x][y];
+    }
+    int minimumNumberOfDeletions(string s) { 
+        // code here
+        string b=s;
+        reverse(b.begin(),b.end());
+        int m = s.size();
+	    int n = b.size();
+	    int len = lcs( m, n,s, b);
+	    return (m-len);
+    } 
+};
+
+// { Driver Code Starts.
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        string S;
+        cin >> S;
+        Solution obj;
+        cout << obj.minimumNumberOfDeletions(S) << endl;
+    }
+    return 0;
+} 
