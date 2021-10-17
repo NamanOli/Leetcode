ee01b87ee95191d7d5bec13b4784d126c6d937a4
Naman Oli <53824950+NamanOli@users.noreply.github.com>
2021-10-17 16:01:37 +0530

    7. longest-k-unique-characters-substring.cpp

---
+++ Sliding Window/7. longest-k-unique-characters-substring.cpp
@@ -0,0 +1,47 @@
+//https://practice.geeksforgeeks.org/problems/longest-k-unique-characters-substring0853/1#
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function template for C++
+
+class Solution{
+  public:
+    int longestKSubstr(string s, int k) {
+    // your code here
+     int i=0; int j= 0;
+       int mx=-1;
+       unordered_map<char,int>mp;
+       while(j<s.size()){
+           mp[s[j]]++;
+           
+           if(mp.size()==k){
+               mx= max(j-i+1, mx);
+               }
+               if(mp.size()>k){
+                   mp[s[i]]--;
+                   if(mp[s[i]]==0)
+                   mp.erase(s[i]);
+                   i++;
+               }
+               j++;
+               
+       }
+       return mx;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string s;
+        cin >> s;
+        int k;
+        cin >> k;
+        Solution ob;
+        cout << ob.longestKSubstr(s, k) << endl;
+    }
+}
